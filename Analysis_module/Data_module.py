3a7a6e02aa5b3fd8487c405039d256c6b77501f8
armandolezama <armando.lezama.psic@gmail.com>
2024-05-15 01:01:56 -0500

    feat(py): added loading data functions

--- Analysis_module/Data_module.py
+++ Analysis_module/Data_module.py
@@ -1,11 +1,29 @@
 from typing import Dict
+from sklearn.datasets import load_boston
+from sklearn.datasets import load_iris
+from sklearn.datasets import fetch_openml
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_digits
 import pandas as pd
 import numpy as np
+import pandas as pd
 
 class Data_module:
   def __init__(self) -> None:
+    self.load_data()
     self.apple_quality_data = pd.read_csv('./Data/apple_quality.csv')
     self.student_data = pd.read_csv('./Data/student_data.csv',header=0, sep=';')
+
+  def load_data(self):
+     self.boston_housing_x, self.boston_housing_y = load_boston(return_X_y=True)
+     self.iris_x, self.iris_y = load_iris(return_X_y=True)
+     self.minist_x, self.minist_y = fetch_openml('mnist_784', version=1, return_X_y=True)
+     self.mall_customer_x = self.load_mall_customer()
+     self.breast_cancer_x, self.breast_cancer_y = load_breast_cancer(return_X_y=True)
+     self.digits_x, self.digits_y = load_digits(return_X_y=True)
+
+  def load_mall_customer(self):
+    return pd.read_csv('./Data/Mall_Customers.csv')
 
   def make_bootstraps(self, data: np.array, n_bootstraps: int=100) -> Dict[str, Dict[str, np.array]]:
 
